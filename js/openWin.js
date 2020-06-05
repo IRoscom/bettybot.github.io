7fa9846950f9e97d3c134fb439c4f2c6c0b35efe
IRoscom <53787123+IRoscom@users.noreply.github.com>
2020-06-06 00:27:14 +1000

    add

--- js/openWin.js
+++ js/openWin.js
@@ -1,3 +1,7 @@
 function openWin() {
 	window.open("https://discord.com/oauth2/authorize?client_id=613017357995999245&scope=bot&permissions=268823616", "window_name", "width=400, height=640");
 }
+
+function openWinSupport() {
+	window.open("http://bettybot.xyz/support", "window_name", "width=648, height=640");
+}
